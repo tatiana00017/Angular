586b8fcaef37d3a659b30de96590ecb937dfdc1c
Tatiana Campos <tatis00017@gmail.com>
2025-08-24 22:47:27 -0500

    ADD componente zapatillas

--- aprendiendo-angular/src/app/app.ts
+++ aprendiendo-angular/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
-
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, VideojuegoComponent],
+  imports: [RouterOutlet, VideojuegoComponent, ZapatillasComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
