c39467af41e2b511ea4a37a44a99d59b74aa77e3
Tatiana Campos <tatis00017@gmail.com>
2025-08-24 21:06:24 -0500

    ADD componente videojuego

--- aprendiendo-angular/src/app/app.ts
+++ aprendiendo-angular/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VideojuegoComponent } from './videojuego/videojuego.componente'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VideojuegoComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ aprendiendo-angular/src/app/videojuego/videojuego.componente.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'videojuego',
+    template: `
+    <h2>Componente videojuegos</h2>
+    <ul>
+        <li>GTA</li>
+        <li>Prince of Persia</li>
+        <li>Tekken</li>
+        <li>Mario</li>
+    </ul>
+    `
+})
+
+export class VideojuegoComponent{
+    
+}
